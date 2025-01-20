71906836cf0ac0c270815d9f99e96b60cf4744b3
Егор Коваленко <egor_oren@bk.ru>
2025-01-21 01:01:10 +0500

    work in progress

--- Task_03_05/Program.cs
+++ Task_03_05/Program.cs
@@ -7,7 +7,16 @@
         время работы программы*/
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Диапазон изменения температуры в градусах Цельсия и шаг изменения температуры. ");
+            int a1 = int.Parse(Console.ReadLine());  int a2 = int.Parse(Console.ReadLine()); int shag = int.Parse(Console.ReadLine());
+            for (int i = 0; a1 + shag < a2; i += shag)
+            {
+                Console.WriteLine((a1 + i) * 1.8 + 32);
+                if (a1 + i >= a2) 
+                {
+                    break;
+                }
+            }
         }
     }
 }
