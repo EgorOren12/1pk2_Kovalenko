61230c0cc97f04844af8535e0b88cd1db139c140
Егор Коваленко <egor_oren@bk.ru>
2025-01-20 23:01:38 +0500

    выполнена 4 задача 2 урока

--- Task_02_04/Program.cs
+++ Task_02_04/Program.cs
@@ -7,23 +7,48 @@
     этом*/
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите свой год рождения");
-            int yearbirt = int.Parse(Console.ReadLine());
+            int yearbirt, monthbirt, daybirt, curyear, curmonth,curday;
+                Console.WriteLine("Введите свой год рождения");
+            while (!int.TryParse(Console.ReadLine(), out yearbirt))
+            {
+                Console.WriteLine("Некорректный ввод, введите год рождения в виде числа");
+            }
+
             Console.WriteLine("Введите свой месяц рождения(числом)");
-            int monthbirt = int.Parse(Console.ReadLine());
+            while (!int.TryParse(Console.ReadLine(), out monthbirt))
+            {
+                Console.WriteLine("Некорректный ввод, введите месяц рождения в виде числа");
+            }
             Console.WriteLine("Введите свой день рождения");
-            int daybirt = int.Parse(Console.ReadLine());
+            while (!int.TryParse(Console.ReadLine(), out daybirt))
+            {
+                Console.WriteLine("Некорректный ввод, введите день рождения в виде числа");
+            }
+            Console.WriteLine("Введите текущий год");
+            while (!int.TryParse(Console.ReadLine(), out curyear))
+            {
+                Console.WriteLine("Некорректный ввод, введите текущий год  в виде числа");
+            }
+            Console.WriteLine("Введите текущий месяц(числом)");
+            while (!int.TryParse(Console.ReadLine(), out curmonth))
+            {
+                Console.WriteLine("Некорректный ввод, введите текущий месяц в виде числа");
+            }
 
-            int curyear = int.Parse(Console.ReadLine()); int curmonth = int.Parse(Console.ReadLine()); int curday = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите текущий день");
+            while (!int.TryParse(Console.ReadLine(), out curday))
+            {
+                Console.WriteLine("Некорректный ввод, введите текущий день в виде числа");
+            }
+
             int Age = curyear - yearbirt;
-            if ((curmonth < monthbirt & Age == 18) || (curmonth == monthbirt & curday < daybirt))
-                Age -= 1;
-            Console.WriteLine(Age);
-            if (Age >= 18)
-                Console.WriteLine("Вы совершеннолетний");
-            else
-                Console.WriteLine("Вы несовершеннолетний");
-
+                if ((curmonth < monthbirt & Age == 18) || (curmonth == monthbirt & curday < daybirt))
+                    Age -= 1;
+                Console.WriteLine(Age);
+                if (Age >= 18)
+                    Console.WriteLine("Вы совершеннолетний");
+                else
+                    Console.WriteLine("Вы несовершеннолетний");
         }
     }
 }
