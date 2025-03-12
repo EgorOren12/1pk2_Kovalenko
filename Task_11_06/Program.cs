cba0017a7924768f032db38b5d31b988731cb456
Егор Коваленко <egor_oren@bk.ru>
2025-03-13 00:08:33 +0500

    выполнена 6 задача 11 урока

---
+++ Task_11_06/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task_11_06
+{
+    internal class Program
+    /*Передача массива по значению: Напишите метод, который принимает массив целых чисел и
+изменяет его элементы, увеличивая каждый на 1. Проверьте, изменился ли оригинальный
+массив вне метода.
+*/
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Введите количество чисел в массиве:");
+            while (!int.TryParse(Console.ReadLine(), out n))
+                Console.WriteLine("Ошибка ввода");
+            int[] numbers = new int[n];
+            Console.Clear();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Введите число {i+1}:");
+                numbers[i] = int.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+
+            Console.WriteLine("Изначальный массив:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] += 1;
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine("\nМассив внутри метода:");
+            Array(numbers);
+        }
+        /// <summary>
+        /// изменение элементов массива на +1
+        /// </summary>
+        /// <param name="array">исходный массив</param>
+        static void Array(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += 1;
+                Console.Write(array[i]+" ");
+            }
+        }
+    }
+}
