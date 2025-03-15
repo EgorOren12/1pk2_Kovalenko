15fcc189a6724009434b58e77d0d82bd39b8b293
Егор Коваленко <kowalenko@bk.ru>
2025-03-15 21:51:23 +0500

    выполнена 2 задача 13 урока

---
+++ Task_13_02/Pet.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_02
+{
+    internal class Pet
+    {
+        #region поля
+        private string name;
+        private string kind;
+        private string age;
+        private string weight;
+        private string health;
+        #endregion
+
+        #region св-ва
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value != null)
+                    name = value;
+                else Console.WriteLine("ОШИБКА! ИМЯ ОТСУТСТВУЕТ");
+
+            }
+        }
+        public string Kind
+        {
+            get { return kind; }
+            set
+            {
+                if (value != null)
+                    kind = value;
+                else
+                Console.WriteLine("ОШИБКА! ОТСУТСТВУЕТ ВИД ЖИВОТНОГО");
+            }
+        }
+        public string Age
+        {
+
+            get { return age; }
+            set
+            {
+                if (value != null)
+                    age = value;
+                else
+                    Console.WriteLine("ОШИБКА! ОТСУТСТВУЕТ ВОЗРАСТ ЖИВОТНОГО");
+            }
+        }
+        public string Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value != null)
+                    weight = value;
+                else
+                    Console.WriteLine("ОШИБКА! ОТСУТСТВУЕТ ВЕС ЖИВОТНОГО");
+            }
+        }
+        public string Health
+        {
+            get { return health; }
+            set
+            {
+                if (value != null)
+                    health = value;
+                else
+                    Console.WriteLine("ОШИБКА! ОТСУТСТВУЕТ ОТМЕТКА О СОСТОЯНИИ ЖИВОТНОГО");
+            }
+        }
+        #endregion
+    
+    #region методы
+   /// <summary>
+   /// возвращает текущую информацию об объекте
+   /// </summary>
+   /// <returns>строка с информацией</returns>
+    public string GetInfoAnimal()
+        {
+            return $"Кличка животного - {name}\nВид - {kind}" +
+                $"\nВозраст - {age}\nВес - {weight}\nОтметка о состоянии здоровья - {health}";
+        }
+        /// <summary>
+        /// изменение веса объекта
+        /// </summary>
+        /// <param name="a">новый вес объекта</param>
+        public void WeightChange(string a)
+        {
+            Weight = a;
+        }
+        /// <summary>
+        /// изменение состояния здоровья объекта
+        /// </summary>
+        /// <param name="a">новое состояние здоровья объекта</param>
+        public void HealthChange(string a)
+        {
+            Health = a;
+        }
+
+        #endregion
+        #region конструкторы
+        public Pet(string name, string kind, string age, string weight, string health)
+        {
+            this.name = name;
+            this.kind = kind;
+            this.age = age;
+            this.weight = weight;
+            this.health = health;
+        }
+        public Pet(string name, string kind, string age, string weight)
+        {
+            this.name=name;
+            this.kind = kind;
+            this.age = age;
+            this.weight = weight;
+        }
+        public Pet(string name, string kind, string age)
+        {
+            this.name = name;
+            this.kind = kind;
+            this.age = age;
+        }
+        public Pet(string name,string kind)
+        {
+            this.name = name;
+            this.kind = kind;
+        }
+        public Pet(string name)
+        {
+            this.name = name;
+        }
+        public Pet() { }
+        #endregion
+    }
+}
---
+++ Task_13_02/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_13_02
+{
+    internal class Program
+    {
+        /*создать класс питомца
+        свойства:
+        кличка, вид животного, возраст, вес, отметка о состонии здоровья (здоров/нездоров)
+        методы:
+        вывод информации об объекте
+        изменение значения веса животного
+        изменение отметки о состоянии здоровья
+        конструторы:
+        предусмотрите разные варианты инициализации объектов*/
+        static void Main(string[] args)
+        {
+        Pet pet1 = new Pet(); //объект 1
+            pet1.Name = "Снежок";
+            pet1.Kind = "Кот";
+            pet1.Age = "7";
+            pet1.Weight = "13";
+            pet1.Health = "Здоров";
+            Console.WriteLine(pet1.GetInfoAnimal());
+            Console.WriteLine();
+            //изменение веса и сост здоровья
+            pet1.WeightChange("20");
+            pet1.HealthChange("Не здоров");
+            Console.WriteLine(pet1.GetInfoAnimal());
+            Console.WriteLine();
+            //объект 2 с отсутствующими параметрами
+            Pet pet2 = new Pet();
+            pet2.Name = "Бенита Сановна";
+            pet2.Kind = "Собака";
+            pet2.Age = null;
+            pet2.Weight = null;
+            pet2.Health = "Здорова";
+            Console.WriteLine(pet2.GetInfoAnimal());
+        }
+    }
+}
