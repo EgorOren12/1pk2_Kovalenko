75ebb02c0be1832ef72633fcce6e9512568d8169
Егор Коваленко <egor_oren@bk.ru>
2025-03-19 01:00:44 +0500

    выполнена 3 задача 14 урока

---
+++ Task_14_03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Channels;
+
+namespace Task_14_03
+{
+    internal class Program
+    {
+        /*Реализуйте статический метод Factorial, который принимает целое число и возвращает его факториал. 
+         * Сделайтетак, чтобы метод работал только для неотрицательных чисел*/
+        static void Main(string[] args)
+        {
+            long n;
+            Console.WriteLine("Введите число");
+            while (!long.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+            if (n < 0)
+                Console.WriteLine("Факториал числа определен только для положительных чисел");
+            else
+            Console.WriteLine($"Факториал числа {n} = {Factorial(n)}");
+            static long Factorial(long a)
+            {
+               long factorial = 1;
+               for (int i = 1; i <= a; i++)
+               {
+                        factorial *= i;
+               }         
+               return factorial;
+            }
+        }
+    }
+}
