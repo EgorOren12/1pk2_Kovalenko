f499effa09cb3f69101b3b37b5109a7044f2ff78
Егор Коваленко <kowalenko@bk.ru>
2025-03-19 21:34:28 +0500

    выполнена 4 задача 14 урока

---
+++ Task_14_04/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_14_04
+{
+    internal class Program
+    {
+        /*Определите класс User, который будет иметь статическое свойство CurrentUser, 
+         * представляющее текущего пользователя, и метод для его установки.*/
+        static void Main(string[] args)
+        {
+            User user1 = new User();
+            User.SetCurrentUser("Андрей");
+            Console.WriteLine(User.CurrentUser);
+
+            User user2 = new User();
+            User.SetCurrentUser("Дэйнерис Таргариен");
+            Console.WriteLine(User.CurrentUser);
+        }
+    }
+}
---
+++ Task_14_04/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_04
+{
+    class User
+    {
+        public static string CurrentUser
+        {
+            get; set;
+        }
+        public static void SetCurrentUser(string a)
+        {
+            CurrentUser = a;
+        }
+    }
+}
