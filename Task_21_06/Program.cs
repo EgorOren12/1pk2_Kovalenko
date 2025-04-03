5dad4eb15bebfb1aa1b1763417cdf853dc37ca61
Егор Коваленко <egor_oren@bk.ru>
2025-04-03 23:42:02 +0500

    выполнена 6 задача 21 урока

---
+++ Task_21_06/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_21_06
+{
+    internal class Program
+    {
+        /*Написать метод, который удаляет повторяющиеся элементы из списка, сохраняя порядок*/
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 20; i++)
+               numbers.Add(rnd.Next(1,20));
+            Console.WriteLine("Начальный список:");
+            foreach(var num in numbers)
+                Console.Write(num+" ");
+            Console.WriteLine("\nСписок после очистки:");
+            numbers = RemoveNumbers(numbers);
+            foreach(var num in numbers)
+                Console.Write(num+" ");
+
+        }
+        static List<int> RemoveNumbers(List<int> numbers)
+        {
+            List<int> resultnumbers = new List<int>();
+            foreach(var num in numbers)
+            {
+                if(!resultnumbers.Contains(num))
+                    resultnumbers.Add(num);
+
+            }
+            return resultnumbers;
+        }
+    }
+}
