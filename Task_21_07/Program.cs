2cc557860468b420a586a067ebdb724c20076ec5
Егор Коваленко <egor_oren@bk.ru>
2025-04-04 22:50:23 +0500

    Выполнена 7 задача 21 урока

---
+++ Task_21_07/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_21_07
+{
+    internal class Program
+    {
+        /*Написать метод, который находит первый ключ в словаре, 
+         * соответствующий заданному значению. Если значения нет, вернуть null.*/
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionaty = new Dictionary<string, string>()
+            {
+                {"Обама", "Барак" },
+                {"Горбачев","Михаил"},
+                {"Романов","Михаил" },
+                {"Рюрикович","Иван" }
+            };
+            Console.WriteLine("Введите искомое значение:");
+            string value = Console.ReadLine();
+            Console.WriteLine(FoundKey(dictionaty,value));
+        }
+
+        static string FoundKey(Dictionary<string, string> dictionary, string currentValue)
+        {
+            foreach (var value in dictionary)
+            {
+                if (value.Value.Equals(currentValue))
+                {
+                    return value.Key;
+                }
+            }
+            return null;
+        }
+    }
+}
