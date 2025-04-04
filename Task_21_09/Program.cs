04c9fc25546711a2c8a95e6715af3ce35b026750
Егор Коваленко <egor_oren@bk.ru>
2025-04-05 00:10:36 +0500

    выполнено 9 задание 21 урока

---
+++ Task_21_09/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Task_21_09
+{
+    internal class Program
+    {
+        /*создайте класс билета (Ticket). сгенерируйте список из 30 билетов. произведите:
+        • поиск билета с максимальной суммой,
+        • билета с минимальной суммой,
+        • выведите список билетов с багажом
+        • выведите список люготных билетов*/
+        static void Main(string[] args)
+        {
+            Ticket ticket;
+            List<Ticket> tickets = new List<Ticket>();
+            for (int i = 0; i <= 30; i++) 
+            {
+            ticket = new Ticket();
+            tickets.Add(ticket);
+                ticket.TicketGetInfo();
+            }
+
+            Console.WriteLine("\nБилет с максимальной суммой:");
+            int maxSumm = 0;
+            foreach (var tic in tickets)
+            {
+                if (tic.Summ > maxSumm)
+                    maxSumm = tic.Summ;
+            }
+            foreach (var tic in tickets)
+            {
+                if (tic.Summ == maxSumm)
+                tic.TicketGetInfo();
+            }
+
+            Console.WriteLine("\nБилет с минимальной суммой:");
+            int minSumm = maxSumm;
+            foreach (var tic in tickets)
+                if (tic.Summ < minSumm)
+                    minSumm = tic.Summ;
+            foreach (var tic in tickets)
+                if (tic.Summ == minSumm)
+                    tic.TicketGetInfo();
+
+            Console.WriteLine("\nБилеты с багажом:");
+            foreach (var tic in tickets)
+                if (tic.Baggaje == true)
+                    tic.TicketGetInfo();
+
+            Console.WriteLine("\nБилеты с льготами:");
+            foreach (var tic in tickets)
+                if (tic.Benefits == true)
+                    tic.TicketGetInfo();
+
+        }
+    }
+}
---
+++ Task_21_09/Ticket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_09
+{
+    internal class Ticket
+    {
+        public int Summ {  get; set; }
+        public bool Baggaje { get; set; }
+        public bool Benefits { get; set; }
+
+        public Ticket() 
+        {
+        Random rnd = new Random();
+            Summ = rnd.Next(1000,5000);
+            int randomBaggaje = rnd.Next(0, 2);
+            if (randomBaggaje == 0)
+                Baggaje = true;
+            else
+                Baggaje = false;
+
+            int randomBenefits = rnd.Next(0, 2);
+            if (randomBenefits == 0)
+                Benefits = true;
+            else
+                Benefits = false;
+        }
+        public void TicketGetInfo()
+        {
+            Console.WriteLine($"Билет на сумму - {Summ}, Багаж? - {Baggaje}, Льготный? - {Benefits}");
+        }
+    }
+}
