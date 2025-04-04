810a49a2ce0781b311c474c51299ee2281426905
Егор Коваленко <egor_oren@bk.ru>
2025-04-04 23:10:12 +0500

    Выполнена 8 задача 21 урока

---
+++ Task_21_08/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_21_08
+{
+    internal class Program
+    {
+        /*напишите метод, который на вход получается массив параметров (строк) и возвращает толькоуникальныестроки*/
+        static void Main(string[] args)
+        {
+            string[] strings = { "Москва", "Уфа", "Пенза", "Питер", "Москва", "Уфа", "Казань","Пенза" };
+            
+            Console.WriteLine("Начальные строки - "+(string.Join(", ",strings)));
+            string[] uniqueStrings = UniqueStrings(strings);
+            Console.WriteLine("Уникальные строки - "+(string.Join(", ",uniqueStrings)));
+        }
+
+        public static string[] UniqueStrings(params string[] strings)
+        {
+           SortedSet<string> strings1 = new SortedSet<string>();
+            foreach (string s in strings) 
+            {
+            strings1.Add(s);
+            }
+            return strings1.ToArray();
+        }
+    }
+}
