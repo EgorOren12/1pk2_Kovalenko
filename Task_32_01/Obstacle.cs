f98d633fbd7f6581c5e20b213a16cb8ccd388d6c
Егор Коваленко <kowalenko@bk.ru>
2025-04-27 21:21:57 +0500

    ООП колобок

---
+++ Task_32_01/Obstacle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_32_01
+{
+    public abstract class Obstacle
+    {
+        public string Name { get; set; }
+        protected Obstacle(string name)
+        {
+            Name = name;
+        }
+        public abstract bool CanKill(Kolobok kolobok);
+    }
+}
