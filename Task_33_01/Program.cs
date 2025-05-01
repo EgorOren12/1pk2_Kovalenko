8be4c0e949386a3228e3a83f9ff97f53bec005b8
Егор Коваленко <kowalenko@bk.ru>
2025-05-01 21:58:01 +0500

    выполнено

---
+++ Task_33_01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Channels;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<IDrawable> list = new List<IDrawable>
+                {
+                       new Circle(5),
+                       new Square(2,7),
+                       new Circle(10),
+                       new Rectangle(7),
+                       new Circle(2),
+                       new Rectangle(10)
+
+                };
+
+                
+                foreach (var item in list)
+                    item.Draw();
+
+                list.Add(new Circle(0));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+
+           
+            
+        }
+    }
+}
