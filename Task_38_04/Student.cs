c0afad29545cd900d5aaeb0ec31e6a759343a9ba
Егор Коваленко <kowalenko@bk.ru>
2025-05-18 02:59:42 +0500

    выполнено

---
+++ Task_38_04/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_38_04
+{
+    class Student : IComparable
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Patronymic { get; private set; }
+        public DateTime Birthday { get; private set; }
+        public string Grooup { get; private set; }
+        public StudentFlour Flour { get; private set; }
+        
+        public Student(string name, string surname,string patronymic,string group, DateTime birthday, StudentFlour flour)
+        {
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            Grooup = group;
+            Birthday = birthday;
+            Flour = flour;
+        }
+
+        public override string ToString()
+        {
+            return $"{Surname} {Name} {Patronymic}";
+        }
+
+        public string FlourOfStudent()
+        {
+            switch (Flour)
+            {
+                case StudentFlour.Male: return "Мужской"; break;
+                case StudentFlour.Female: return "Женский"; break;
+            }
+            return null;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj != null && obj is Student student)
+            {
+                return (Surname+Name).CompareTo(student.Surname+ student.Name);
+            }
+            else
+                throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ Task_38_04/Students.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Task_38_04
+{
+    class Students
+    {
+        private List<Student> students = new();
+        public List<Student> StudentsList => students;
+        private string fileName = "studentsSaveFile";
+        public void AddStudent(Student student)
+        {
+            if (student != null)
+            {
+                students.Add(student);
+                students.Sort();
+            }
+        }
+        public void RemoveStudent(Student student)
+        {
+            if (students.Contains(student))
+            {
+                students.Remove(student);
+            }
+        }
+
+        public void Save()
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+            var seriolizion = JsonSerializer.Serialize(students,options);
+            File.WriteAllText(fileName, seriolizion);
+        }  
+
+        public void Load()
+        {
+            if (!File.Exists(fileName))
+                return;
+            var data = File.ReadAllText(fileName);
+            if (data != null)
+                students = JsonSerializer.Deserialize<List<Student>>(data);
+            else students = new List<Student>();
+        }
+    }
+}
